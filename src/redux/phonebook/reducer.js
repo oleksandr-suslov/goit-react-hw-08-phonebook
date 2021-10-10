1093b21a71912a3d95a4b25e473b0068211a899a
oleksandr-suslov <Oleksandr.Suslov@gmail.com>
2021-10-11 00:10:34 +0300

    remove 'error' on state

--- src/redux/phonebook/reducer.js
+++ src/redux/phonebook/reducer.js
@@ -11,24 +11,11 @@ const getContacts = createReducer([], {
     state.filter((el) => el.id !== action.payload),
 });
 
-const error = createReducer(null, {
-  [fetchContacts.rejected]: (state, action) =>
-    action.error.message ?? action.error,
-  [fetchContacts.pending]: () => null,
-  [addContacts.rejected]: (state, action) =>
-    action.error.message ?? action.error,
-  [addContacts.pending]: () => null,
-  [removeContact.rejected]: (state, action) =>
-    action.error.message ?? action.error,
-  [removeContact.pending]: () => null,
-});
-
 const filter = createReducer("", {
   [filterContact]: (state, action) => action.payload,
 });
 
 export default combineReducers({
   contacts: getContacts,
-  error,
   filter,
 });
